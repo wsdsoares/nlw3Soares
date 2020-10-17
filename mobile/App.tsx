cdfbd5e22d2a1808fc998aade0559ec4cfc75fb1
Willian Soares Damasceno <wilhaods@gmail.com>
2020-10-17 13:05:13 -0300

    Mobile - Versão 01.

---
+++ mobile/App.tsx
@@ -0,0 +1,81 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Assistindo jogo do cruzeiro!</Text>
+      <MapView 
+        provider={PROVIDER_GOOGLE}
+        style={styles.map} 
+        initialRegion={{
+          latitude: -17.0023877,
+          longitude: -46.0063049,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+        >
+          <Marker 
+            icon={mapMarker}
+            calloutAnchor={{
+              x: 2.87,
+              y: 0.8,
+            }}
+            coordinate={{
+              latitude: -17.0023877,
+              longitude: -46.0063049,
+            }}
+          >
+            <Callout tooltip onPress={()=> {}}>
+              <View style={styles.calloutContainer}>
+                <Text style={styles.calloutText}>Lar das meminas</Text>
+
+              </View>
+            </Callout>
+
+          </Marker>
+        </MapView>
+
+        <View style={styles.footer}>
+          <Text style={styles.footerText}></Text>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer:{
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText:{
+    color: '#0089a5',
+    fontSize: 14,
+  },
+
+  footer: {
+
+  },
+
+  footerText: {
+    
+  }
+});
